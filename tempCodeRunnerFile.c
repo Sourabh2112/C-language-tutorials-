4bbc6dd1d4380ce4b8b563fc2891a8771114d91b
sourabh kumar <sourabhsbg01@gmail.com>
2021-12-24 00:36:24 +0530

    information about string libraries

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,23 +1,2 @@
 #include<stdio.h>
-
-
-// int main()
-// {
-//         int D;
-//         printf("Enter the value of D\n");
-//         scanf("%d", &D);
-
-//       while (2)
-//       {
-          
-//            float a;
-//            printf("Enter the value of a :");
-//            scanf("\n%f", &a);
-//            printf("The value is %f", a/D);
-//            printf("\n");
-//       }
-      
-       
-
-//     return 0;
-// }
+#include<string.h>
